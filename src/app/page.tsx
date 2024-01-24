f17949c044652867abefa6769227915f06a1996f
Faraz Khan <farazkhanaa2555@gmail.com>
2024-01-24 16:38:26 +0500

    feat: completed my document section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import AboutSection from '@/shared/components/about-section/about-section'
+import DocumentSection from '@/shared/components/document-section/document-section'
 import Header from '@/shared/components/header/page'
 import PreSaleStatusContainer from '@/shared/components/pre-sale-status-bar/pre-sale-staus-bar'
 
@@ -8,6 +9,7 @@ export default function Home() {
       <Header />
       <PreSaleStatusContainer />
       <AboutSection />
+      <DocumentSection />
     </main>
   )
 }
